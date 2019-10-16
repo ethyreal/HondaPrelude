1fbcc7fb46d5770d7a52a4505eeebf26fea06cbd
George Webster <george@ethyreal.com>
2019-10-15 17:20:57 -0700

    Initial Commit

---
+++ HondaPrelude/HondaPrelude.h
@@ -0,0 +1,19 @@
+//
+//  HondaPrelude.h
+//  HondaPrelude
+//
+//  Created by George Webster on 10/15/19.
+//  Copyright © 2019 George Webster. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HondaPrelude.
+FOUNDATION_EXPORT double HondaPreludeVersionNumber;
+
+//! Project version string for HondaPrelude.
+FOUNDATION_EXPORT const unsigned char HondaPreludeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HondaPrelude/PublicHeader.h>
+
+
